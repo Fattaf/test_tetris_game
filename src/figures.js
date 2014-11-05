271e1449c7ec63215de69b08378f19b9c365d2a0
fattaf <andrew.nashkolniy@gmail.com>
2014-11-05 19:36:30 +0200

    add jquery

--- src/figures.js
+++ src/figures.js
@@ -1,25 +1,51 @@
 
 var Figure = function() {
-  this.position     = [50, 50];
+  this.position     = [1, 1];
   this.figure_size  = [40, 40];
+  this.max_disp     = 3;
   this.part_disps   = NaN;
   this.color        = NaN;
 
   this.drawFigure = function(context) {
     context.beginPath();
     for(var i = 0; i < this.part_disps.length; i++) {
-      context.rect( this.position[0] + this.part_disps[i][0],
-                    this.position[1] + this.part_disps[i][1],
+      context.rect( (this.position[0] + this.part_disps[i][0]) * this.figure_size[0],
+                    (this.position[1] + this.part_disps[i][1]) * this.figure_size[0],
                     this.figure_size[0],
                     this.figure_size[1] );
     };
     context.fillStyle = this.color;
     context.fill();
-    context.lineWidth = 3;
+    context.lineWidth = 2;
     context.strokeStyle = 'black';
     context.stroke();
   };
-  return this;
+
+  // FIXME: too big rotation !!
+  this.turn = function() {
+    var tmp_disps = [];
+    for(var i = 0; i < this.part_disps.length; i++) {
+      var element1 = this.part_disps[i][1],
+          element2 = this.max_disp - this.part_disps[i][0] - 1;
+      tmp_disps.push([element1, element2])
+    };
+    this.part_disps = tmp_disps;
+  };
+
+  this.pull_left = function() {
+    // check if figure still on field
+    this.position[0] -= 1;
+  };
+  this.pull_right = function() {
+    // check if figure still on field
+    this.position[0] += 1;
+  };
+  this.pull_down = function() {
+    // check if figure still on field
+    // check if figure is not on built part
+    this.position[1] += 1;
+  };
+
 };
 
 var FigureBuilder = function() {
@@ -36,49 +62,49 @@ var FigureBuilder = function() {
   // * * *
   //     *
   var figureType1 = function(figure) {
-    figure.part_disps = [[0, 0], [40, 0], [80, 0], [80, 40]];
+    figure.part_disps = [[0, 0], [1, 0], [2, 0], [2, 1]];
     figure.color = 'yellow';
     return figure;
   };
   // * * *
   //   *
   var figureType2 = function(figure) {
-    figure.part_disps = [[0, 0], [40, 0], [80, 0], [40, 40]];
+    figure.part_disps = [[0, 0], [1, 0], [2, 0], [1, 1]];
     figure.color = '#1487F8';
     return figure;
   };
   // * *
   //   * *
   var figureType3 = function(figure) {
-    figure.part_disps = [[0, 0], [40, 0], [40, 40], [80, 40]];
+    figure.part_disps = [[0, 0], [1, 0], [1, 1], [2, 1]];
     figure.color = '#0EEB1F';
     return figure;
   };
   // * *
   // * *
   var figureType4 = function(figure) {
-    figure.part_disps = [[0, 0], [40, 0], [40, 40], [0, 40]];
+    figure.part_disps = [[0, 0], [1, 0], [1, 1], [0, 1]];
     figure.color = '#FA4C23';
     return figure;
   };
   // * * * *
   //
   var figureType5 = function(figure) {
-    figure.part_disps = [[0, 0], [40, 0], [80, 0], [120, 0]];
+    figure.part_disps = [[0, 0], [1, 0], [2, 0], [3, 0]];
     figure.color = '#DF30D9';
     return figure;
   };
   // * * *
   // *
   var figureType6 = function(figure) {
-    figure.part_disps = [[0, 0], [40, 0], [80, 0], [0, 40]];
-    figure.color = '#F50000';  //'#E9181C';
+    figure.part_disps = [[0, 0], [1, 0], [2, 0], [0, 1]];
+    figure.color = '#F50000';
     return figure;
   };
   //   * *
   // * *
   var figureType7 = function(figure) {
-    figure.part_disps = [[40, 0], [80, 0], [0, 40], [40, 40]];
+    figure.part_disps = [[1, 0], [2, 0], [0, 1], [1, 1]];
     figure.color = '#93E508';
     return figure;
   };
