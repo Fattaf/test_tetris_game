4f92483744ca5306b4ae6d23d38fbc04aa377ec4
fattaf <andrew.nashkolniy@gmail.com>
2014-12-04 15:45:36 +0200

    refactoring draw_adapter class

--- src/draw_adapter.js
+++ src/draw_adapter.js
@@ -1,45 +1,43 @@
 var DrawAdapter = function(canvas_div) {
-  this.canvas = canvas_div;
-  this.context = this.canvas.getContext('2d');
+  var canvas  = canvas_div;
+  var context = canvas.getContext('2d');
 
   this.draw_image = function(src) {
     var imageObj = new Image();
     var self = this;
 
-    imageObj.onload = function() {
-      self.context.drawImage(imageObj, 69, 50);
-    };
+    imageObj.onload = function() { context.drawImage(imageObj, 69, 50); };
     imageObj.src = src;
   };
 
   this.clear_canvas = function() {
-    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    context.clearRect(0, 0, canvas.width, canvas.height);
   };
 
+  // FIXME: change font
   this.addScore = function(score) {
-    // FIXME: change font
-    this.context.font = 'italic 30pt Calibri';
-    this.context.fillText('Score: ' + score, 420, 100);
+    context.font = 'italic 30pt Calibri';
+    context.fillText('Score: ' + score, 420, 100);
   };
 
   this.addBorder = function(start_point, end_point, color, lineWidth) {
-    this.context.beginPath();
-    this.context.rect(start_point[0] - 3,
-                      start_point[1] - 3,
-                      end_point[0] + 6,
-                      end_point[1] + 6 );
-    this.addStroke(this.context, color, lineWidth);
+    context.beginPath();
+    context.rect(start_point[0] - 3,
+                 start_point[1] - 3,
+                 end_point[0] + 6,
+                 end_point[1] + 6 );
+    this.addStroke(context, color, lineWidth);
   };
 
   this.addFill = function(color) {
-    this.context.fillStyle = color;
-    this.context.fill();
+    context.fillStyle = color;
+    context.fill();
   };
 
   this.addStroke = function(color, lineWidth) {
-    this.context.lineWidth = lineWidth;
-    this.context.strokeStyle = color;
-    this.context.stroke();
+    context.lineWidth = lineWidth;
+    context.strokeStyle = color;
+    context.stroke();
   };
 
   this.addFigure = function(figure, null_x, null_y) {
@@ -47,41 +45,49 @@ var DrawAdapter = function(canvas_div) {
     if (null_x === undefined) { null_x = 10; };
     if (null_y === undefined) { null_y = 10; };
 
-    this.context.beginPath();
+    var new_x = null,
+        new_y = null;
+
+    context.beginPath();
     for(var i = 0; i < figure.body.length; i++) {
-      var new_x = null_x + Math.abs((figure.position[0] + figure.body[i][0]) * figure.figure_size[0]),
-          new_y = null_y + Math.abs((figure.position[1] + figure.body[i][1]) * figure.figure_size[1]);
-      this.context.rect(new_x, new_y, figure.figure_size[0], figure.figure_size[1]);
+      new_x = null_x + Math.abs((figure.position[0] + figure.body[i][0]) * figure.figure_size[0]),
+      new_y = null_y + Math.abs((figure.position[1] + figure.body[i][1]) * figure.figure_size[1]);
+      context.rect(new_x, new_y, figure.figure_size[0], figure.figure_size[1]);
     };
   };
 
   this.addFieldBackground = function(field) {
-    this.context.beginPath();
-    for (var i = 0; i < field.miniMap.length; i++) {
-      for(var j = 0; j < field.miniMap[i].length; j++) {
-        this.context.rect((field.disp * j) + field.position[0],
-                          (field.disp * i) + field.position[1],
-                          40, 40);
-      };
-    };
+    context.beginPath();
+    forEachInMatrix(field, function(field, i, j) {
+      buildRectangle(field, i, j);
+    });
     this.addFill('#FFFFFF');
     this.addStroke('#EDEDED', 1);
   };
 
   this.drawFieldMarkedCells = function(field) {
-    this.context.beginPath();
-    for (var i = 0; i < field.miniMap.length; i++) {
-      for(var j = 0; j < field.miniMap[i].length; j++) {
-        if (field.miniMap[i][j] == 1){
-          this.context.rect((field.disp * j) + field.position[0],
-                            (field.disp * i) + field.position[1],
-                            40, 40);
-        };
-      };
-    };
+    context.beginPath();
+    forEachInMatrix(field, function(field, i, j) {
+      if (field.miniMap[i][j] == 1) { buildRectangle(field, i, j); };
+    });
     this.addFill('#9ED0FF');
     this.addStroke('black', 3);
   };
+
+  // private methods
+    var buildRectangle = function(field, i, j) {
+      context.rect((field.disp * j) + field.position[0],
+                   (field.disp * i) + field.position[1],
+                   40, 40);
+    };
+
+    var forEachInMatrix = function(field, block) {
+      for (var i = 0; i < field.miniMap.length; i++) {
+        for(var j = 0; j < field.miniMap[i].length; j++) {
+          block(field, i, j);
+        };
+      };
+    };
 
   return this;
 };
